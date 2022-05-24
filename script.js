ad48900bd9535b5a41485ad7f2670e763d6303ce
Nick <86365071+Fitsos17@users.noreply.github.com>
2022-05-24 20:19:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const endpoint = "https://api.adviceslip.com/advice";
+const adviceNumber = document.querySelector(".advice-number");
+const adviceText = document.querySelector(".advice-text");
+const button = document.querySelector("button");
+
+function getRandomAdvice() {
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      adviceNumber.textContent = `Advice #${data.slip.id}`;
+      adviceText.textContent = `"${data.slip.advice}"`;
+    });
+  return;
+}
+
+button.addEventListener("click", () => {
+  getRandomAdvice();
+});
+
+getRandomAdvice();
